a0288c3157bb005584dadb66ead190a6b291ceb5
Shekar <chandrasekhar.allam@mutualmobile.com>
2017-03-02 15:51:40 +0530

    Removed attach detach from Presenter implementation

--- app/src/main/java/com/shekar/alamomvp/ui/category/CategoryPresenter.java
+++ app/src/main/java/com/shekar/alamomvp/ui/category/CategoryPresenter.java
@@ -14,54 +14,40 @@ import rx.subscriptions.CompositeSubscription;
  */
 public class CategoryPresenter extends BasePresenter<CategoryMvpView> {
 
-    private CategoryMvpView mCategoryMvpView;
-    private CompositeSubscription compositeSubscription;
-    private DataManager mDataManager;
+  private CompositeSubscription compositeSubscription;
+  private DataManager mDataManager;
 
-    public CategoryPresenter(DataManager dataManager) {
-        mDataManager = dataManager;
+  public CategoryPresenter(DataManager dataManager) {
+    mDataManager = dataManager;
+  }
+
+  public void loadData() {
+    if (compositeSubscription == null) {
+      compositeSubscription = new CompositeSubscription();
     }
+    getMvpView().showProgress();
+    compositeSubscription.add(mDataManager.getCategorys()
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Subscriber<List<CategoryModel>>() {
+          @Override public void onCompleted() {
+          }
 
-    @Override
-    public void attachView(CategoryMvpView categoryMvpView) {
-        mCategoryMvpView = categoryMvpView;
-    }
+          @Override public void onError(Throwable e) {
+            getMvpView().hideProgress();
+            getMvpView().showError();
+          }
 
-    public void loadData() {
-        if (compositeSubscription == null) {
-            compositeSubscription = new CompositeSubscription();
-        }
-        mCategoryMvpView.showProgress();
-
-        compositeSubscription.add(mDataManager.getCategorys().observeOn(AndroidSchedulers.mainThread()).subscribe(new Subscriber<List<CategoryModel>>() {
-            @Override
-            public void onCompleted() {
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                mCategoryMvpView.hideProgress();
-                mCategoryMvpView.showError();
-            }
-
-            @Override
-            public void onNext(List<CategoryModel> categoryModels) {
-                mCategoryMvpView.hideProgress();
-                mCategoryMvpView.showContent(categoryModels);
-            }
+          @Override public void onNext(List<CategoryModel> categoryModels) {
+            getMvpView().hideProgress();
+            getMvpView().showContent(categoryModels);
+          }
         }));
-    }
+  }
 
-    @Override
-    public void unsubscribeFromDataStore() {
-        Log.d("TAG", "unsubscribeFromDataStore ");
-        compositeSubscription.unsubscribe();
-        compositeSubscription.clear();
-        compositeSubscription = null;
-    }
-
-    @Override
-    public void detachView() {
-        mCategoryMvpView = null;
-    }
+  @Override public void unsubscribeFromDataStore() {
+    Log.d("TAG", "unsubscribeFromDataStore ");
+    compositeSubscription.unsubscribe();
+    compositeSubscription.clear();
+    compositeSubscription = null;
+  }
 }
